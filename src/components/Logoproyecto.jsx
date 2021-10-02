86ba92abc7bfd84d8cce6cecb3655a203368ee0a
Roger Alexis Valencia Garcia <90428541+IngeNeo@users.noreply.github.com>
2021-10-01 23:08:43 -0500

    Revert "8 crear header footer"

--- src/components/Logoproyecto.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Logo from 'media/logo.png';
-
-const Logoproyecto = () => {
-  return <img className='w-auto h-40 mx-auto' src={Logo} alt='Proyecto' />;
-};
-
-export default Logoproyecto;
